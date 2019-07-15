3704753bdf2603ebc14d0127db797794e1803ba5
sweyome <sweyome1@gmail.com>
2019-07-14 20:26:20 -0400

    react todo and form created

---
+++ todo/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Redux from 'react';
+import reducers from './reducers/reducers';
+import Headline from './components/test';
+import {connect} from 'react-redux';
+import TodoForm from './components/ToDoForm/TodoForm';
+import TodoList from './components/TodoList/TodoList';
+import { addText } from './actions/'
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      }
+    }
+  render() {
+    return (
+      <div className="App">
+        <TodoForm />
+        <TodoList />
+      </div>
+    );
+  }
+}
+
+export default connect(()=>({}), {addText})(App)
